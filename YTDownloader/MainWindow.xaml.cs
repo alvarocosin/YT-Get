c18d45833be9f5458b0e51c6acb7d0356ba8c47f
Álvaro Cosín <cosin.alvaro@gmail.com>
2022-12-15 12:18:17 +0100

    Python script and UI update

--- YTDownloader/MainWindow.xaml.cs
+++ YTDownloader/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +24,32 @@ namespace YTDownloader
         public MainWindow()
         {
             InitializeComponent();
+            destPath.Text = "C:/";
+            linkText.Text = "https://www.youtube.com/watch?v=XXXXXXXXX";
+        }
+
+        private void SearchPath_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog openFileDlg = new FolderBrowserDialog();
+            var result = openFileDlg.ShowDialog();
+            if (result.ToString() != string.Empty)
+            {
+                destPath.Text = openFileDlg.SelectedPath;
+            }
+        }
+        private void Download_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("CMD.exe", "/C python ytmp3downloader.py " + linkText.Text + " " + destPath.Text);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void ClickLinkBox(object sender, MouseButtonEventArgs e)
+        {
+            linkText.Text = "";
         }
     }
 }
