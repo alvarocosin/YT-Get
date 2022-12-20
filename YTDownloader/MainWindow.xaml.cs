56fdebd639bbbe29a079ee9b3266b049d4d389a0
Álvaro Cosín <cosin.alvaro@gmail.com>
2022-12-20 11:12:35 +0100

    UI and structure change

--- YTDownloader/MainWindow.xaml.cs
+++ YTDownloader/MainWindow.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
-using System.Windows.Media;
+using System.Windows.Media; 
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
